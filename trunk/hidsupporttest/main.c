a93bcc3fc1f2526ac8dd23036210daeff838bbf2
matthias.ringwald@gmail.com <matthias.ringwald@gmail.com@9f8e5dcc-dcae-8f76-5cb7-b310d4c9e470>
2011-01-26 23:31:18 +0000

    minimal test that swipes left & right

---
+++ trunk/hidsupporttest/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+#include <unistd.h>
+
+#include <hid-support.h>
+
+void hid_inject_mouse_rel_move_test(int buttons, float dx, float dy){
+	printf("Button %u, dx = %f, dy = %f\n", buttons, dx, dy);
+}
+
+int main(int argc, char **argv, char **envp) {
+	
+	printf("Simulating left & right swipes until CTRL-C\n");
+	
+	float last_x = 0;
+	
+	hid_inject_mouse_rel_move(0, 160, 240);
+	hid_inject_mouse_rel_move(1, 0, 0);
+	float angle = 0;
+	while (1){
+		sleep(1);
+		angle += 30;
+		float new_x = sin( angle / 180.0 * 3.14159);
+		hid_inject_mouse_rel_move(1, (new_x-last_x) * 100, 0);
+		last_x = new_x;
+	}
+	return 0;
+}
+
+// vim:ft=objc
